dcada98253b690f3fa0aec1b4d77df3870a38175
Douglas Sellers <doug@fitorb.com>
2015-05-07 22:06:01 -0700

    It now finds a single face

--- facereplace/FaceReplaceViewController.swift
+++ facereplace/FaceReplaceViewController.swift
@@ -15,14 +15,30 @@ class FaceReplaceViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        findFaceInBaseImage()
     }
+    
+
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func findFaceInBaseImage()
+    {
+        let context:CIContext = CIContext(options:nil)
+        let detector:CIDetector = CIDetector(ofType: CIDetectorTypeFace, context: context, options: [CIDetectorAccuracy : CIDetectorAccuracyHigh])
+        let features:NSArray = [detector.featuresInImage(CIImage(image: mainImage.image))]
+        for subArray in features
+        {
+            for f in subArray as! NSArray
+            {
+                let face:CIFaceFeature = f as! CIFaceFeature
+                NSLog("Face found at (%f,%f) of dimensions %fx%f", face.bounds.origin.x, face.bounds.origin.y, face.bounds.width, face.bounds.height);
+            }
+        }
     }
     
 
